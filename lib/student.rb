6b1fa8daac651854ea55068d4d4cb5dd203396e9
Emily Morgan <emorgan05@gmail.com>
2017-10-24 00:06:55 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -102,4 +102,15 @@ class Student
       self.new_from_db(row)
     end.first
   end
+
+  def self.all_students_in_grade_X(num)
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = ?
+    SQL
+
+    DB[:conn].execute(sql, num).map do |row|
+      self.new_from_db(row)
+    end
+  end
 end
